15a0d4727058647e991f82db39661e8cc282fabf
Harijs Deksnis <harijs.deksnis@gmail.com>
2016-05-14 14:32:50 +0200

    weather command returns a promise

--- fbMessengerBot/commands/weather.cmd.js
+++ fbMessengerBot/commands/weather.cmd.js
@@ -4,26 +4,49 @@ var weather = require('weather-js');
 module.exports = function (commandArguments) {
 
 
-    weather.find({search: commandArguments, degreeType: 'C'}, function(err, result) {
-          
-        if (err) {
+    return new Promise(function (resolve, reject) {
+
+
+        var a;
+
+        weather.find({search: commandArguments, degreeType: 'C'}, function(err, result) {
+             
+            var result = result[0];
+           
+            if (err) {
+                
+                a = new fbMessage
+                    .PlainText("Couldn't find wather for " + commandArguments)
+                    .compose();
+
+            } else {
+
+                var temperature = parseInt(result.current.temperature) < 0 ? '-' + result.current.temperature : '+' + result.current.temperature + ' C';
+
+                a = new fbMessage
+                    .GenericTemplate()
+                    .addElement({
+                        title:      result.current.skytext,
+                        image_url:  result.current.imageUrl,
+                        subtitle:   temperature + ' Wind: ' + result.current.winddisplay
+                    })
+                    .compose();
+
+            }
             
-            var textReply = new fbMessage
-                .PlainText("Couldn't find wather for " + commandArguments)
-                .compose();
 
-        } else {
+            resolve(a);
 
-            var textReply = new fbMessage
-                .PlainText(JSON.stringify(result))
-                .compose();
+        });
 
-        }
-        
 
-        return textReply; 
 
 
     });
+
+
+
+
+
 
 }
--- fbMessengerBot/handleSlashCommands.js
+++ fbMessengerBot/handleSlashCommands.js
@@ -39,7 +39,21 @@ module.exports = function (config) {
         var restCommandParameters = _.rest(commands, 1).join(' ');
         var commandResult = availableCommands[commands[0]](restCommandParameters);
 
-        sendMessage(senderId, commandResult); 
+        //it's a promise
+        if (commandResult.then) {
+            
+            commandResult.then(function (result) {
+                
+                sendMessage(senderId, result); 
+
+            });
+
+
+        } else {
+            sendMessage(senderId, commandResult); 
+        }
+
+        
 
     } else {
         
